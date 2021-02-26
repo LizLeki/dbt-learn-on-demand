e01b9366975b9f92f10d3f115c578d1967799415
Liz Leki <elizabethleki@gmail.com>
2021-02-26 20:33:22 +0000

    add ttl spend

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,6 +6,14 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 
+spend as (
+    select 
+        customer_id, 
+        sum(amount) as lifetime_value 
+        from {{ ref('fct_orders') }} 
+        group by customer_id
+        ),
+
 customer_orders as (
 
     select
@@ -19,23 +27,15 @@ customer_orders as (
 
     group by 1
 
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
 )
-
-select * from final
+select
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name,
+    customer_orders.first_order_date,
+    customer_orders.most_recent_order_date,
+    coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+    spend.lifetime_value
+    from customers
+    left join customer_orders using (customer_id)
+    left join spend using (customer_id)
