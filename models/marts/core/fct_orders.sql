0f8179f435803a6835cd14a97a7798087f4a1de5
Liz Leki <elizabethleki@gmail.com>
2021-02-26 20:24:54 +0000

    fixed sql error

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -3,12 +3,12 @@ select
     customer_id,
     order_date,
     coalesce(amount, 0) as amount
-from select * from {{ ref('stg_orders' )}}
+from (select * from {{ ref('stg_orders' )}})
 left join (
     select
         order_id,
         sum(case when status = 'success' then amount end) as amount
     from {{ ref('stg_payments') }}
     group by order_id
-    )
+    ) o
 using (order_id)
